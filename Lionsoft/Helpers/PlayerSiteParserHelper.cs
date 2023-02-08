094ff8db29784ae8a30f3d8e9dbfc5abeabc289b
Jakub Lewandowski <jakub.lewandowski@paypro.pl>
2023-02-08 13:17:53 +0100

    Poprawa pobierania danych gracza

--- Lionsoft/Helpers/PlayerSiteParserHelper.cs
+++ Lionsoft/Helpers/PlayerSiteParserHelper.cs
@@ -5,19 +5,25 @@ namespace Lionsoft.Helpers
 {
     public static class PlayerSiteParserHelper
     {
+        public static IEnumerable<PlayerDataModel> GetPlayersData()
+        {
+            yield return GetPlayerData(31483, 38681);
+            yield return GetPlayerData(31492, 38542);
+        }
+
         public static PlayerDataModel GetPlayerData(int psid, int bo5id)
         {
             var url = $"https://ranking.polskisquash.pl/info/org.gracz/{psid}";
             var playerDataModel = new PlayerDataModel()
             {
-                 PsLink = url,
-                 Bo5Link = $"https://gracz.squasha.pl/{bo5id}/"
+                 PsLink = url
             };
             var http = new HttpClient();
             var webData = http.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(webData);
             playerDataModel.Name = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"ranking2\"]/div/div[1]/div/h2/span[2]").InnerText;
+            playerDataModel.Bo5Link = $"https://gracz.squasha.pl/{bo5id}/{playerDataModel.Name.Replace(" ", "+")}";
             int i = 0;
             var rankingModel = new RankingModel();
 
